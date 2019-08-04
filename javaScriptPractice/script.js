1d82351b3ef205c475720e69b97352b7cbe1aa37
Sushant Mongia <sushantmongia@gmail.com>
2019-08-04 15:16:12 -0400

    Section 8 Done

---
+++ javaScriptPractice/script.js
@@ -0,0 +1,15 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log (fullAge);
+
+var job = 'Teacher';
+console.log(job);
+
+var _rainMan = 'Rain Man';
+var $mainMan = 'Main Man';
+console.log(_rainMan, $mainMan);
