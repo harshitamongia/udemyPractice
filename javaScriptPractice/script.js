7873c9d79860437511a9ce3fd2dc7499264743ce
Sushant Mongia <sushantmongia@gmail.com>
2019-09-19 15:03:44 -0400

    refreshedSep19

--- javaScriptPractice/script.js
+++ javaScriptPractice/script.js
@@ -112,3 +112,63 @@ console.log(BMIMark);
 var isMarkHigher = BMIMark > BMIJohn;
 console.log("Is Mark's BMI greater thatn John's ? " + isMarkHigher);
 */
+
+
+/* If else
+*/
+
+/*
+var fisrtName = "John"
+var civilStatus = 'single'
+
+if (civilStatus === 'married'){
+    console.log(fisrtName + ' is married!');
+} else{
+    console.log(fisrtName + ' will hopefullly marry soon :) ');
+}
+
+var isMarried = true;
+if (isMarried){
+    console.log(fisrtName + ' is married!');
+} else{
+    console.log(fisrtName + ' will hopefullly marry soon :) ');
+}
+
+var heightJohn = 1.7;
+var heightMark = 1.6;
+var massJohn = 50;
+var massMark = 60;
+
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIJohn);
+var BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIMark);
+
+if (BMIMark > BMIJohn){
+    console.log('Mark\'s BMI is greater than John\'s BMI ')
+} else {
+    console.log('John\'s BMI is greater than Mark\'s BMI ')
+}
+
+*/
+
+
+// Boolean Logic
+
+// var firstName = 'John';
+// var age = 19;
+
+// if (age < 13){
+//     console.log(firstName + ' is a boy.');
+// } else if ( age >= 13 && age < 20){ // Between 13 and 20 === age >= 13 AND age < 20
+//     console.log(firstName + ' is a teenager.')
+// } else if ( age >= 20 && age < 30) {
+//     console.log(firstName + ' is a young man')
+// } else {
+//     console.log(firstName + ' is a man.');
+// }
+
+
+
+var firstName = 'John';
+var age = 14;
